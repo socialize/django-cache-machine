d3a65dbe2dd71f71e5b3ee909d39ab4bd89ffac3
Isaac Mosquera <imosquer@gmail.com>
2012-11-06 17:10:24 -0800

    adding params for tests

--- fabfile.py
+++ fabfile.py
@@ -37,11 +37,11 @@ try:
 except Exception:
     print 'WARNING: Skipping redis tests.'
 
-def test():
+def test(test_params=""):
     for settings in SETTINGS:
         print settings
         os.environ['DJANGO_SETTINGS_MODULE'] = 'cache-machine.%s' % settings
-        local('django-admin.py test --failfast --verbosity 2')
+        local('django-admin.py test %s' % test_params)
 
 
 def updoc():
